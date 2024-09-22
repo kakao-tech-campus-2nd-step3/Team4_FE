0376cfb3bdb95f9d4da7ac602938df40480b14b3
seorinnn <srgongju@naver.com>
2024-09-22 18:24:41 +0900

    delete : 불필요한 Fragment 태그 삭제

--- src/pages/Signup/UserSignup.tsx
+++ src/pages/Signup/UserSignup.tsx
@@ -3,77 +3,75 @@ import styled from '@emotion/styled';
 
 export const UserSignupPage = () => {
   return (
-    <>
-      <Wrapper>
-        <TitleWrapper>
-          <Text fontSize='2xl' as='b' mt='13px' mb='20px' ml='8px'>
-            회원가입
-          </Text>
-        </TitleWrapper>
+    <Wrapper>
+      <TitleWrapper>
+        <Text fontSize='2xl' as='b' mt='13px' mb='20px' ml='8px'>
+          회원가입
+        </Text>
+      </TitleWrapper>
 
-        <FormWrapper>
-          <FormControl id='username' isRequired>
-            <FormLabel>이메일</FormLabel>
-            <Input
-              type='email'
-              placeholder='이메일을 입력해주세요'
-              focusBorderColor='#FF1658'
-              mb='10px'
-            />
-          </FormControl>
+      <FormWrapper>
+        <FormControl id='username' isRequired>
+          <FormLabel>이메일</FormLabel>
+          <Input
+            type='email'
+            placeholder='이메일을 입력해주세요'
+            focusBorderColor='#FF1658'
+            mb='10px'
+          />
+        </FormControl>
 
-          <FormControl id='password' isRequired>
-            <FormLabel>비밀번호</FormLabel>
-            <Input
-              type='password'
-              placeholder='비밀번호를 입력해주세요'
-              focusBorderColor='#FF1658'
-              mb='10px'
-            />
-          </FormControl>
+        <FormControl id='password' isRequired>
+          <FormLabel>비밀번호</FormLabel>
+          <Input
+            type='password'
+            placeholder='비밀번호를 입력해주세요'
+            focusBorderColor='#FF1658'
+            mb='10px'
+          />
+        </FormControl>
 
-          <FormControl id='confirm-password' isRequired>
-            <FormLabel>비밀번호 확인</FormLabel>
-            <Input
-              type='password'
-              placeholder='비밀번호를 다시 입력해주세요'
-              focusBorderColor='#FF1658'
-              mb='10px'
-            />
-          </FormControl>
+        <FormControl id='confirm-password' isRequired>
+          <FormLabel>비밀번호 확인</FormLabel>
+          <Input
+            type='password'
+            placeholder='비밀번호를 다시 입력해주세요'
+            focusBorderColor='#FF1658'
+            mb='10px'
+          />
+        </FormControl>
 
-          <FormControl id='name' isRequired>
-            <FormLabel>이름</FormLabel>
-            <Input
-              type='text'
-              placeholder='이름을 입력해주세요'
-              focusBorderColor='#FF1658'
-              mb='10px'
-            />
-          </FormControl>
+        <FormControl id='name' isRequired>
+          <FormLabel>이름</FormLabel>
+          <Input
+            type='text'
+            placeholder='이름을 입력해주세요'
+            focusBorderColor='#FF1658'
+            mb='10px'
+          />
+        </FormControl>
 
-          <FormControl id='region' isRequired>
-            <FormLabel>지역</FormLabel>
-            <Input
-              type='text'
-              placeholder='지역을 입력해주세요 (예 : OO시 OO구)'
-              focusBorderColor='#FF1658'
-              mb='10px'
-            />
-          </FormControl>
+        <FormControl id='region' isRequired>
+          <FormLabel>지역</FormLabel>
+          <Input
+            type='text'
+            placeholder='지역을 입력해주세요 (예 : OO시 OO구)'
+            focusBorderColor='#FF1658'
+            mb='10px'
+          />
+        </FormControl>
 
-          <Button
-            bg='#FF1658'
-            color='#F5F5F5'
-            _hover={{ bg: '#FF467E' }}
-            size='md'
-            mt='24px'
-          >
-            회원가입
-          </Button>
-        </FormWrapper>
-      </Wrapper>
-    </>
+        <Button
+          bg='#FF1658'
+          color='#F5F5F5'
+          _hover={{ bg: '#FF467E' }}
+          size='md'
+          mt='24px'
+        >
+          회원가입
+        </Button>
+      </FormWrapper>
+    </Wrapper>
   );
 };
 
