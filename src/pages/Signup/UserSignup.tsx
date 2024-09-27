cd5685e5c7c69d0415a5f352f8de71a1f19ae31b
seorinnn <srgongju@naver.com>
2024-09-27 19:46:36 +0900

    feat : UserSignup.tsx 파일에 회원가입 기능 구현

--- src/pages/Signup/UserSignup.tsx
+++ src/pages/Signup/UserSignup.tsx
@@ -1,8 +1,62 @@
 import { Text, Input, FormControl, FormLabel, Button } from '@chakra-ui/react';
-import { Wrapper, TitleWrapper, FormWrapper } from './TrainerSignup.styles';
+import { Wrapper, TitleWrapper, FormWrapper } from './UserSignup.styles';
+import { useSignup } from '@/hooks/useSignup';
+import { useState } from 'react';
+import { UserSignupFormData } from '@/types';
 
 export const UserSignupPage = () => {
-  const userType = 'general';
+  const registerType = 'user';
+  const { handleSignup } = useSignup(registerType);
+
+  const [formData, setFormData] = useState<UserSignupFormData>({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    name: '',
+    profileImage: null,
+    local: '',
+  });
+
+  //값이 변경될 때 호출되는 함수
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value, files } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [id]: files ? files[0] : value, // 파일 필드 처리
+    }));
+  };
+
+  //회원가입 버튼을 눌렀을 때 호출되는 함수
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // 비밀번호와 비밀번호 확인이 일치하는지 확인
+    if (formData.password !== formData.passwordConfirm) {
+      alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
+      return;
+    }
+
+    // JSON 데이터를 Blob으로 변환
+    const jsonData = {
+      email: formData.email,
+      password: formData.password,
+      name: formData.name,
+      local: formData.local,
+    };
+    const jsonBlob = new Blob([JSON.stringify(jsonData)], {
+      type: 'application/json',
+    });
+
+    // FormData 생성 및 Blob, 파일 추가
+    const formDataToSend = new FormData();
+    formDataToSend.append('user', jsonBlob); // JSON 데이터를 Blob으로 추가
+
+    if (formData.profileImage) {
+      formDataToSend.append('profileImage', formData.profileImage); // 파일 추가
+    }
+
+    await handleSignup(formDataToSend);
+  };
 
   return (
     <Wrapper>
@@ -11,80 +65,93 @@ export const UserSignupPage = () => {
           회원가입
         </Text>
       </TitleWrapper>
+      <form onSubmit={onSubmit}>
+        <FormWrapper>
+          <FormControl id='email' isRequired>
+            <FormLabel>이메일</FormLabel>
+            <Input
+              type='email'
+              placeholder='이메일을 입력해주세요'
+              focusBorderColor='#FF1658'
+              mb='10px'
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </FormControl>
 
-      <FormWrapper>
-        <FormControl id='username' isRequired>
-          <FormLabel>이메일</FormLabel>
-          <Input
-            type='email'
-            placeholder='이메일을 입력해주세요'
-            focusBorderColor='#FF1658'
-            mb='10px'
-          />
-        </FormControl>
+          <FormControl id='password' isRequired>
+            <FormLabel>비밀번호</FormLabel>
+            <Input
+              type='password'
+              placeholder='비밀번호를 입력해주세요'
+              focusBorderColor='#FF1658'
+              mb='10px'
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </FormControl>
 
-        <FormControl id='password' isRequired>
-          <FormLabel>비밀번호</FormLabel>
-          <Input
-            type='password'
-            placeholder='비밀번호를 입력해주세요'
-            focusBorderColor='#FF1658'
-            mb='10px'
-          />
-        </FormControl>
+          <FormControl id='passwordConfirm' isRequired>
+            <FormLabel>비밀번호 확인</FormLabel>
+            <Input
+              type='password'
+              placeholder='비밀번호를 다시 입력해주세요'
+              focusBorderColor='#FF1658'
+              mb='10px'
+              value={formData.passwordConfirm}
+              onChange={handleChange}
+            />
+          </FormControl>
 
-        <FormControl id='confirm-password' isRequired>
-          <FormLabel>비밀번호 확인</FormLabel>
-          <Input
-            type='password'
-            placeholder='비밀번호를 다시 입력해주세요'
-            focusBorderColor='#FF1658'
-            mb='10px'
-          />
-        </FormControl>
+          <FormControl id='name' isRequired>
+            <FormLabel>이름</FormLabel>
+            <Input
+              type='text'
+              placeholder='이름을 입력해주세요'
+              focusBorderColor='#FF1658'
+              mb='10px'
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </FormControl>
 
-        <FormControl id='name' isRequired>
-          <FormLabel>이름</FormLabel>
-          <Input
-            type='text'
-            placeholder='이름을 입력해주세요'
-            focusBorderColor='#FF1658'
-            mb='10px'
-          />
-        </FormControl>
+          <FormControl id='local' isRequired>
+            <FormLabel>지역</FormLabel>
+            <Input
+              type='text'
+              placeholder='지역을 입력해주세요 (예 : OO시 OO구)'
+              focusBorderColor='#FF1658'
+              mb='10px'
+              value={formData.local}
+              onChange={handleChange}
+            />
+          </FormControl>
 
-        <FormControl id='region' isRequired>
-          <FormLabel>지역</FormLabel>
-          <Input
-            type='text'
-            placeholder='지역을 입력해주세요 (예 : OO시 OO구)'
-            focusBorderColor='#FF1658'
-            mb='10px'
-          />
-        </FormControl>
+          <FormControl id='profileImage'>
+            <FormLabel>프로필 이미지</FormLabel>
+            <Input
+              type='file'
+              accept='image/*'
+              mb='10px'
+              border='none'
+              _hover={{ border: 'none' }}
+              _focus={{ border: 'none' }}
+              onChange={handleChange}
+            />
+          </FormControl>
 
-        <FormControl id='profile-image'>
-          <FormLabel>프로필 이미지</FormLabel>
-          <Input
-            type='file'
-            accept='image/*'
-            mb='10px'
-            border='none'
-            _hover={{ border: 'none' }}
-            _focus={{ border: 'none' }}
-          />
-        </FormControl>
-
-        <Button
-          bg='#FF1658'
-          color='#F5F5F5'
-          _hover={{ bg: '#FF467E' }}
-          size='md'
-          mt='24px'
-        >
-          회원가입
-        </Button>
-      </FormWrapper>
+          <Button
+            bg='#FF1658'
+            color='#F5F5F5'
+            _hover={{ bg: '#FF467E' }}
+            size='md'
+            mt='24px'
+            type='submit'
+          >
+            회원가입
+          </Button>
+        </FormWrapper>
+      </form>
     </Wrapper>
   );
 };
