75094d027127b0695d509fff4d2d68ec0c9a713b
seorinnn <srgongju@naver.com>
2024-10-03 20:48:35 +0900

    feat : 로그인한 회원 유형별로 다른 홈화면 보여주는 기능 구현

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,20 @@
+import { useAuth } from '@/hooks/useAuth';
+import { UserHomePage } from './UserHome';
+import { TrainerHomePage } from './TrainerHome';
+
 export const HomePage = () => {
+  const { isAuthenticated, type } = useAuth();
+
+  const renderHomeScreen = () => {
+    switch (type) {
+      case 'user':
+        return <UserHomePage />;
+      case 'trainer':
+        return <TrainerHomePage />;
+    }
+  };
+
   return (
-    <>
-      <h1>HomePage</h1>
-    </>
+    <div>{isAuthenticated ? renderHomeScreen() : <p>로그인 전 홈화면</p>}</div>
   );
 };
