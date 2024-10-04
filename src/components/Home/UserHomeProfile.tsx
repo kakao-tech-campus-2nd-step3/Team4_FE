c00afb21ab00992038b0ab17bc6987f69603ce48
seorinnn <srgongju@naver.com>
2024-10-04 20:08:15 +0900

    refactor : 일반회원 프로필 가져오는 userProfile 커스텀훅으로 분리

--- src/components/Home/UserHomeProfile.tsx
+++ src/components/Home/UserHomeProfile.tsx
@@ -5,26 +5,13 @@ import {
   StyledProfileImage,
   StyledNameText,
 } from './UserHomeProfile.styles';
-import { useEffect, useState } from 'react';
-import { UserProfile } from '@/types';
-import { getUserProfile } from '@/api/profile/userProfile';
+import { useProfile } from '@/hooks/useProfile';
 
 export const UserHomeProfile = () => {
-  const [profile, setProfile] = useState<UserProfile>();
-  useEffect(() => {
-    const fetchProfile = async () => {
-      try {
-        const data = await getUserProfile();
-        setProfile(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  const profile = useProfile();
 
-    fetchProfile();
-  }, []);
-
-  if (!profile) {
+  // profile이 UserProfile 타입인지 확인
+  if (!profile || !('profileImageUrl' in profile)) {
     return <p>프로필 정보를 불러오는 중...</p>;
   }
 
