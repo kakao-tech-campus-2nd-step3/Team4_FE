6ec28609a297484b85e7b57cd771765a20c988ef
seorinnn <srgongju@naver.com>
2024-10-04 09:56:30 +0900

    feat : UserHomeProfile 컴포넌트 구현

---
+++ src/components/Profile/UserHomeProfile.styles.ts
@@ -0,0 +1,15 @@
+import { CardBody } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  margin-top: 30px;
+`;
+
+export const StyledCardBody = styled(CardBody)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+`;
