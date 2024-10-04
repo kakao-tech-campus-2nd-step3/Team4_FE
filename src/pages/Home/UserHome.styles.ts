866899b9c36f73a802a2b4e6df94ed1f7d84d723
seorinnn <srgongju@naver.com>
2024-10-04 09:59:04 +0900

    style : UserHomePage 스타일 수정

--- src/pages/Home/UserHome.styles.ts
+++ src/pages/Home/UserHome.styles.ts
@@ -1,3 +1,12 @@
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const UserHomeProfileWrapper = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`;
