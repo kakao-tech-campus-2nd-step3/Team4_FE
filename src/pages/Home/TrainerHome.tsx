f62a58bc5c51948d1b1cb3db6a4c2d6518624f80
seorinnn <srgongju@naver.com>
2024-10-06 00:12:06 +0900

    feat : 트레이너회원 홈화면에 프로필 추가

--- src/pages/Home/TrainerHome.tsx
+++ src/pages/Home/TrainerHome.tsx
@@ -1,9 +1,10 @@
+import { TrainerHomeProfile } from '@/components/Home/TrainerHomeProfile';
 import { Wrapper } from './UserHome.styles';
 
 export const TrainerHomePage = () => {
   return (
     <Wrapper>
-      <p>트레이너회원 홈화면</p>
+      <TrainerHomeProfile />
     </Wrapper>
   );
 };
