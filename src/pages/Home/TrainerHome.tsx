ba3264ecd6eaf6ef7ee338ce58e7d5efc3e21c78
seorinnn <srgongju@naver.com>
2024-10-06 19:34:15 +0900

    feat : 트레이너회원 홈화면에 내 고객 찾기 & 내 제안 기록 & 리뷰 보기 컴포넌트 추가

--- src/pages/Home/TrainerHome.tsx
+++ src/pages/Home/TrainerHome.tsx
@@ -1,10 +1,36 @@
 import { TrainerHomeProfile } from '@/components/Home/TrainerHomeProfile';
 import { Wrapper } from './UserHome.styles';
+import { FindClient } from '@/components/Home/FindClient';
+import { ProposalHistory } from '@/components/Home/ProposalHistory';
+import { Reviews } from '@/components/Home/Reviews';
+import {
+  FindClientWrapper,
+  ProposalHistoryWrapper,
+  ReviewsWrapper,
+  RightWrapper,
+  TrainerHomeProfileWrapper,
+  TrainerHomePTWrapper,
+} from './TrainerHome.styles';
 
 export const TrainerHomePage = () => {
   return (
     <Wrapper>
-      <TrainerHomeProfile />
+      <TrainerHomeProfileWrapper>
+        <TrainerHomeProfile />
+      </TrainerHomeProfileWrapper>
+      <TrainerHomePTWrapper>
+        <FindClientWrapper>
+          <FindClient />
+        </FindClientWrapper>
+        <RightWrapper>
+          <ProposalHistoryWrapper>
+            <ProposalHistory />
+          </ProposalHistoryWrapper>
+          <ReviewsWrapper>
+            <Reviews />
+          </ReviewsWrapper>
+        </RightWrapper>
+      </TrainerHomePTWrapper>
     </Wrapper>
   );
 };
