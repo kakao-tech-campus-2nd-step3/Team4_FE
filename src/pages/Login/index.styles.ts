53c7ee99316965dc501475cc0a4e65a2b0600512
seorinnn <srgongju@naver.com>
2024-10-07 17:11:17 +0900

    style : 로그인화면 스타일 수정

--- src/pages/Login/index.styles.ts
+++ src/pages/Login/index.styles.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
   display: flex;
-  justify-content: spcae-between;
+  justify-content: space-between;
   margin: 60px auto;
 `;
 
@@ -11,14 +11,27 @@ export const LoginWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100%;
 `;
 
 export const TitleWrapper = styled.div`
   display: flex;
+  justify-content: center;
+  width: 100%;
 `;
 
 export const FormWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 80%;
+  width: 100%;
+`;
+
+export const StyledForm = styled.form`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const FormControlWrapper = styled.div`
+  width: 100%;
 `;
