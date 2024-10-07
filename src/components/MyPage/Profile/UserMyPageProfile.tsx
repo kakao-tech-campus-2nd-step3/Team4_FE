8818666cfae3e00d4382840d281d6a0774f5df81
seorinnn <srgongju@naver.com>
2024-10-07 22:49:28 +0900

    feat : 일반회원 마이페이지 프로필 컴포넌트 구현

---
+++ src/components/MyPage/Profile/UserMyPageProfile.tsx
@@ -0,0 +1,27 @@
+import { Card } from '@chakra-ui/react';
+import {
+  Wrapper,
+  StyledCardBody,
+  StyledProfileImage,
+  StyledNameText,
+} from '../../Home/UserHomeProfile.styles';
+import { useProfile } from '@/hooks/useProfile';
+
+export const UserMyPageProfile = () => {
+  const profile = useProfile();
+
+  if (!profile) {
+    return <p>프로필 정보를 불러오는 중...</p>;
+  }
+
+  return (
+    <Wrapper>
+      <Card>
+        <StyledCardBody>
+          <StyledProfileImage src={profile.profileImageUrl} alt='프로필' />
+          <StyledNameText mt='15px'>{profile.name} 님</StyledNameText>
+        </StyledCardBody>
+      </Card>
+    </Wrapper>
+  );
+};
