0263047bb5b55c9dcd17eb31e7e49575a76e5584
seorinnn <srgongju@naver.com>
2024-10-09 00:35:42 +0900

    feat : 트레이너회원 마이페이지에 프로필 및 경력 등록 버튼 추가

--- src/pages/MyPage/TrainerMyPage.tsx
+++ src/pages/MyPage/TrainerMyPage.tsx
@@ -1,3 +1,21 @@
+import { TrainerMyPageProfile } from '@/components/MyPage/Profile/TrainerMyPageProfile';
+import { StyledButton, Wrapper } from './TrainerMyPage.styles';
+import { useNavigate } from 'react-router-dom';
+import { RouterPath } from '@/routes/path';
+
 export const TrainerMyPage = () => {
-  return <p>트레이너 마이페이지</p>;
+  const navigate = useNavigate();
+
+  const navigateToRegisterCareer = () => {
+    navigate(RouterPath.registerCareer);
+  };
+
+  return (
+    <Wrapper>
+      <TrainerMyPageProfile />
+      <StyledButton type='button' onClick={navigateToRegisterCareer}>
+        경력 등록하기
+      </StyledButton>
+    </Wrapper>
+  );
 };
