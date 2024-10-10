1eba38eab6eb734b312fcee72059ff50bfe9ddd1
seorinnn <srgongju@naver.com>
2024-10-10 19:25:16 +0900

    feat : career 등록하는 api 함수 정의

---
+++ src/api/career/postCareer.ts
@@ -0,0 +1,18 @@
+import { Career } from '@/types';
+
+export const postCareer = async (careers: Career[]) => {
+  const token = localStorage.getItem('accessToken');
+
+  const response = await fetch('/api/trainers/career', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(careers),
+  });
+
+  if (response.status !== 201) {
+    throw new Error('경력 업로드 실패');
+  }
+};
