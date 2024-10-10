98d0274afe8f62e0bb859ae01bd53f19f5c2de62
seorinnn <srgongju@naver.com>
2024-10-10 19:24:26 +0900

    fix : try catch문 삭제하여 실패한 경우에도 성공으로 판단되는 문제 해결

--- src/api/bodyInfo/postBodyInfo.ts
+++ src/api/bodyInfo/postBodyInfo.ts
@@ -1,19 +1,15 @@
 export const postBodyInfo = async (formData: FormData) => {
   const token = localStorage.getItem('accessToken');
 
-  try {
-    const response = await fetch('/api/users/bodyInfo', {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      body: formData,
-    });
+  const response = await fetch('/api/users/bodyInfo', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: formData,
+  });
 
-    if (response.status !== 201) {
-      throw new Error('이미지 업로드 실패');
-    }
-  } catch (error) {
-    console.error('업로드 중 오류가 발생했습니다:', error);
+  if (response.status !== 201) {
+    throw new Error('이미지 업로드 실패');
   }
 };
