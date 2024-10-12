b7c9b035b35b7c9d39ee078492c2a9dbdc980700
seorinnn <srgongju@naver.com>
2024-10-12 23:36:28 +0900

    feat : useBodyInfo 커스텀훅에 BodyInfo 데이터 가져오는 로직 및 BodyInfo 데이터 업로드 후 새로고침 로직 추가

--- src/hooks/useBodyInfo.ts
+++ src/hooks/useBodyInfo.ts
@@ -1,11 +1,18 @@
+import { getBodyInfo } from '@/api/bodyInfo/getBodyInfo';
 import { postBodyInfo } from '@/api/bodyInfo/postBodyInfo';
+import { RouterPath } from '@/routes/path';
+import { BodyInfo } from '@/types';
+import { useEffect, useState } from 'react';
 
 export const useBodyInfo = () => {
+  const [bodyInfo, setBodyInfo] = useState<BodyInfo[]>([]);
+
   const handleUploadBodyInfo = async (formData: FormData) => {
     try {
       await postBodyInfo(formData);
 
       alert('이미지가 성공적으로 업로드되었습니다.');
+      window.location.replace(RouterPath.mypage);
       return true;
     } catch (error) {
       console.error('이미지 업로드 실패:', error);
@@ -14,5 +21,18 @@ export const useBodyInfo = () => {
     }
   };
 
-  return { handleUploadBodyInfo };
+  useEffect(() => {
+    const fetchBodyInfo = async () => {
+      try {
+        const data = await getBodyInfo();
+        setBodyInfo(data);
+      } catch (error) {
+        console.error('인바디 이미지 데이터를 가져오는데 실패했습니다.', error);
+      }
+    };
+
+    fetchBodyInfo();
+  }, []);
+
+  return { bodyInfo, handleUploadBodyInfo };
 };
