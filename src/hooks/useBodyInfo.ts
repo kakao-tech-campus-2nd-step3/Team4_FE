8ca706054dac67e30e08cc2ff81f85148af4b638
seorinnn <srgongju@naver.com>
2024-10-13 21:24:32 +0900

    delete : useBodyInfo, useCareer 훅에서 불필요한 return 문 삭제

--- src/hooks/useBodyInfo.ts
+++ src/hooks/useBodyInfo.ts
@@ -13,11 +13,9 @@ export const useBodyInfo = () => {
 
       alert('이미지가 성공적으로 업로드되었습니다.');
       window.location.replace(RouterPath.mypage);
-      return true;
     } catch (error) {
       console.error('이미지 업로드 실패:', error);
       alert('이미지 업로드에 실패했습니다.');
-      return false;
     }
   };
 
