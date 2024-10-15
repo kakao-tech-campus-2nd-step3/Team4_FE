76014ece28a0b49fa4ab2a76b4340981f3929aa0
seorinnn <srgongju@naver.com>
2024-10-15 17:10:17 +0900

    style : RegisterInbodyButton 공통 컴포넌트 정의

---
+++ src/components/Common/RegisterInbodyButton.tsx
@@ -0,0 +1,16 @@
+import { RouterPath } from '@/routes/path';
+import { useNavigate } from 'react-router-dom';
+import { StyledButton } from './RegisterInbodyButton.styles';
+
+export const RegisterInbodyButton = () => {
+  const navigate = useNavigate();
+
+  const navigateToRegisterInbody = () => {
+    navigate(RouterPath.registerBodyInfo);
+  };
+  return (
+    <StyledButton type='button' onClick={navigateToRegisterInbody}>
+      인바디 이미지 등록하기
+    </StyledButton>
+  );
+};
