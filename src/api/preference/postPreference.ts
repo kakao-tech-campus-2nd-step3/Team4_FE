357ee9c0749b3ac176a6fdb53635350699de224e
seorinnn <srgongju@naver.com>
2024-10-17 01:54:09 +0900

    feat : 선호도 등록하여 매칭 신청하는 postPreference api 함수 정의

---
+++ src/api/preference/postPreference.ts
@@ -0,0 +1,10 @@
+import { Preference } from '@/types';
+import { fetchAPI } from '..';
+
+export const postPreference = async (preference: Preference) => {
+  return await fetchAPI({
+    method: 'POST',
+    endpoint: '/preferences',
+    body: preference,
+  });
+};
