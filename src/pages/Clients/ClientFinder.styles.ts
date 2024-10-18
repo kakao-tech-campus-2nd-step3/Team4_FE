eec847d2e7c189c632e2a4df1f78cdbb930cb73b
seorinnn <srgongju@naver.com>
2024-10-18 15:15:24 +0900

    style : 내 고객 찾기 ClientFinder 페이지 스타일 정의

---
+++ src/pages/Clients/ClientFinder.styles.ts
@@ -0,0 +1,14 @@
+import { Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 60px 0;
+`;
+
+export const StyledText = styled(Text)`
+  font-size: 24px;
+  font-weight: 600;
+`;
