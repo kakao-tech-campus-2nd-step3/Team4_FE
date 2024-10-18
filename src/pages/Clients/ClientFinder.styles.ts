3e52d61ee4041812a28487ccc504c10b49ebe304
seorinnn <srgongju@naver.com>
2024-10-18 17:33:26 +0900

    style : ClientFinder 컴포넌트 스타일 수정

--- src/pages/Clients/ClientFinder.styles.ts
+++ src/pages/Clients/ClientFinder.styles.ts
@@ -5,10 +5,15 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 60px 0;
+  margin: 50px 0;
 `;
 
 export const StyledText = styled(Text)`
   font-size: 24px;
   font-weight: 600;
+  margin-bottom: 10px;
+`;
+
+export const StyledDefaultText = styled(Text)`
+  text-align: center;
 `;
