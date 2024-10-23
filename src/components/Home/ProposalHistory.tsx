8132de14434b17ef3791db3af12737c4c6cd47f3
seorinnn <srgongju@naver.com>
2024-10-23 17:55:27 +0900

    feat : 홈화면에서 내 제안 기록 클릭 시 ProposalHistoryPage로 이동하는 navigate 추가

--- src/components/Home/ProposalHistory.tsx
+++ src/components/Home/ProposalHistory.tsx
@@ -8,10 +8,18 @@ import {
   StyledText,
   Wrapper,
 } from './MyPT.styles';
+import { useNavigate } from 'react-router-dom';
+import { RouterPath } from '@/routes/path';
 
 export const ProposalHistory = () => {
+  const navigate = useNavigate();
+
+  const handleProposalHistoryClick = () => {
+    navigate(RouterPath.proposalHistory);
+  };
+
   return (
-    <Wrapper>
+    <Wrapper onClick={handleProposalHistoryClick}>
       <Card height='125px'>
         <StyledCardBody>
           <StyledImage src={ProposalHistoryImage} alt='내 제안 기록' />
