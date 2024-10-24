895e948e1c9d967470ce2dc1e096d9c011513781
seorinnn <srgongju@naver.com>
2024-10-24 22:52:38 +0900

    feat : useProposal 커스텀훅에 fetchTrainerProposals 함수 추가

--- src/hooks/useProposal.ts
+++ src/hooks/useProposal.ts
@@ -1,7 +1,22 @@
+import { getTrainerProposal } from '@/api/proposal/getTrainerProposal';
 import { postProposal } from '@/api/proposal/postProposal';
-import { Proposal } from '@/types';
+import { Proposal, TrainerProposalResponse } from '@/types';
+import { useState } from 'react';
 
 export const useProposal = () => {
+  const [trainerProposals, setTrainerProposals] = useState<
+    TrainerProposalResponse[]
+  >([]);
+  const fetchTrainerProposals = async () => {
+    try {
+      const response = await getTrainerProposal();
+      setTrainerProposals(response);
+    } catch (error) {
+      console.error('PT 제안 리스트를 가져오는 데 실패했습니다:', error);
+      alert('PT 제안 리스트를 가져오는 데 실패했습니다.');
+    }
+  };
+
   const handleUploadProposal = async (proposal: Proposal) => {
     try {
       await postProposal(proposal);
@@ -12,5 +27,6 @@ export const useProposal = () => {
       alert('PT 제안에 실패했습니다.');
     }
   };
-  return { handleUploadProposal };
+
+  return { trainerProposals, fetchTrainerProposals, handleUploadProposal };
 };
